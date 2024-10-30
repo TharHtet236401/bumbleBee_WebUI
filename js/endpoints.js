f69fbdf2c9d1c0a14e327a547d8fedcccfc01ebe
Steve <swemhtet.net@gmail.com>
2024-10-30 20:22:11 +0700

    backup-for-leave-request

--- js/endpoints.js
+++ js/endpoints.js
@@ -15,81 +15,81 @@ const cookieRoute = `${mainApi}/api/cookie`;
 
 
 //Cookie
+export const cookieCheckApi = `${cookieRoute}/check`;
 
-const cookieCheckApi = `${cookieRoute}/check`
+// Auth
+export const registerApi = `${authRoute}/web/register`;
+export const loginApi = `${authRoute}/login`;
+export const pwdResetApi = `${authRoute}/passwordReset`;
+export const changePwdApi = `${authRoute}/changePassword`;
+export const logOutApi = `${authRoute}/logout`;
 
-//Auth 
-const registerApi = `${authRoute}/web/register`
-const loginApi = `${authRoute}/login`
-const pwdResetApi = `${authRoute}/passwordReset`
-const changePwdApi = `${authRoute}/changePassword`
-const logOutApi = `${authRoute}/logout`
+// Class Related functions
+export const createClassApi = `${classRoute}/create`;
+export const deleteClassApi = `${classRoute}/delete`;
+export const editClassApi = `${classRoute}/edit`;
+export const allClassesApi = `${classRoute}/allClasses`;
+export const readByAdminApi = `${classRoute}/readByAdmin`;
+export const readByTrAndGuardianApi = `${classRoute}/readByTeacherAndGuardian`;
+export const gradeNamesApi = `${classRoute}/gradeNames`;
+export const classNamesApi = `${classRoute}/classNames`;
+export const classNamesByTeacherNewApi = (gradeName) => `${classRoute}/classNamesByTeacherNew/${gradeName}`;
+export const gradeNamesByTeacherApi = `${classRoute}/gradeNamesByTeacher`;
+export const classNamesByTeacherApi = `${classRoute}/classNamesByTeacher`;
+export const getClassByIdApi = (classId) => `${classRoute}/classById/${classId}`;
 
-//Class Related functions
-const createClassApi = `${classRoute}/create`
-const deleteClassApi = `${classRoute}/delete`
-const editClassApi = `${classRoute}/edit`
-const allClassesApi = `${classRoute}/allClasses`
-const readByAdminApi = `${classRoute}/readByAdmin`
-const readByTrAndGuardianApi = `${classRoute}/readByTeacherAndGuardian`
-const gradeNamesApi = `${classRoute}/gradeNames`
-const classNamesApi = `${classRoute}/classNames`
-const classNamesByTeacherNewApi = (gradeName) =>  `${classRoute}/classNamesByTeacherNew/${gradeName}`;
-const gradeNamesByTeacherApi = `${classRoute}/gradeNamesByTeacher`;
-const classNamesByTeacherApi = `${classRoute}/classNamesByTeacher`;
-const getClassByIdApi = (classId) => `${classRoute}/classById/${classId}`;
+// Image Related functions
+export const uploadProfilePicture = `${userRoute}/upload-profile-picture`;
+export const uploadProfilePictureSimple = `${userRoute}/upload-profile-picture-simple`;
 
-//Image Related functions
-const uploadProfilePicture = `${userRoute}/upload-profile-picture`;
-const uploadProfilePictureSimple = `${userRoute}/upload-profile-picture-simple`;
+// Leave Request functions
+export const createLeaveRequestApi = `${leaveRequestRoute}/create`;
+export const readLeaveRequestByClassApi = (classId) => `${leaveRequestRoute}/readByClass/${classId}`;
+export const readAllLeaveRequestsApi = `${leaveRequestRoute}/readAllReq`;
+export const editLeaveRequestApi = `${leaveRequestRoute}/edit`;
+export const deleteLeaveRequestApi = `${leaveRequestRoute}/delete`;
+export const respondLeaveRequestApi = `${leaveRequestRoute}/respond`;
+export const getLeaveReasonsApi = `${leaveRequestRoute}/reasons`;
 
-//Leave Request functions
-const createLeaveRequestApi = `${leaveRequestRoute}/create`;
-const readLeaveRequestByClassApi = (classId) => `${leaveRequestRoute}/readByClass/${classId}`;
-const readAllLeaveRequestsApi = `${leaveRequestRoute}/readAllReq`;
-const editLeaveRequestApi = `${leaveRequestRoute}/edit`;
-const deleteLeaveRequestApi = `${leaveRequestRoute}/delete`;
-const respondLeaveRequestApi = `${leaveRequestRoute}/respond`;
-const getLeaveReasonsApi = `${leaveRequestRoute}/reasons`;
+// Post Api
+export const createPostApi = `${postRoute}/create`;
+export const createPostWithProgressApi = `${postRoute}/createWithProgress`;
+export const getPostsApi = `${postRoute}/getPosts`;
+export const getFeedsApi = `${postRoute}/getFeeds`;
+export const getAnnouncementsApi = `${postRoute}/getAnnouncements`;
+export const editPostApi = (postId) => `${postRoute}/edit/${postId}`;
+export const deletePostApi = (postId) => `${postRoute}/delete/${postId}`;
+export const filterFeedsApi = `${postRoute}/filterFeeds`;
 
-//Post Api
-const createPostApi = `${postRoute}/create`;
-const createPostWithProgressApi = `${postRoute}/createWithProgress`;
-const getPostsApi = `${postRoute}/getPosts`;
-const getFeedsApi = `${postRoute}/getFeeds`;
-const getAnnouncementsApi = `${postRoute}/getAnnouncements`;
-const editPostApi = (postId) => `${postRoute}/edit/${postId}`;
-const deletePostApi = (postId) => `${postRoute}/delete/${postId}`;
-const filterFeedsApi = `${postRoute}/filterFeeds`;
+// Request Api
+export const joinClassApi = `${requestRoute}/create`;
+export const readTeacherRequestsApi = `${requestRoute}/readTeacherRequests`;
+export const readGuardianRequestsApi = `${requestRoute}/readGuardianRequests`;
+export const respondTeacherReqApi = `${requestRoute}/respondTeacherReq`;
+export const respondGuardianReqApi = `${requestRoute}/respondGuardianReq`;
 
-//Request Api
-const joinClassApi = `${requestRoute}/create`;
-const readTeacherRequestsApi = `${requestRoute}/readTeacherRequests`;
-const readGuardianRequestsApi = `${requestRoute}/readGuardianRequests`;
-const respondTeacherReqApi = `${requestRoute}/respondTeacherReq`;
-const respondGuardianReqApi = `${requestRoute}/respondGuardianReq`;
+// School Api
+export const createSchoolApi = `${schoolRoute}/create`;
+export const editSchoolApi = `${schoolRoute}/edit`;
+export const getSchoolApi = `${schoolRoute}/getSchool`;
+export const deleteSchoolApi = `${schoolRoute}/delete`;
 
-//School Api
-const createSchoolApi = `${schoolRoute}/create`;
-const editSchoolApi = `${schoolRoute}/edit`;
-const getSchoolApi = `${schoolRoute}/getSchool`;
-const deleteSchoolApi = `${schoolRoute}/delete`;
-
-//Student Api
-const addNewStudentToClassApi = (classId) => `${studentRoute}/add/${classId}`;
-const getStudentsByClassApi = (classId) => `${studentRoute}/get/${classId}`;
-const getStudentsByClassCodeApi = (classCode) => `${studentRoute}/getByClassCode/${classCode}`;
-const getStudentInfoApi = (studentId) => `${studentRoute}/getStudentInfo/${studentId}`;
-const getStudentByParentApi = `${studentRoute}/getStudentByParent`;
-const editStudentApi = (studentId) => `${studentRoute}/edit/${studentId}`;
-const deleteStudentApi = (studentId) => `${studentRoute}/delete/${studentId}`;
+// Student Api
+export const addNewStudentToClassApi = (classId) => `${studentRoute}/add/${classId}`;
+export const getStudentsByClassApi = (classId) => `${studentRoute}/get/${classId}`;
+export const getStudentsByClassCodeApi = (classCode) => `${studentRoute}/getByClassCode/${classCode}`;
+export const getStudentInfoApi = (studentId) => `${studentRoute}/getStudentInfo/${studentId}`;
+export const getStudentByParentApi = `${studentRoute}/getStudentByParent`;
+export const editStudentApi = (studentId) => `${studentRoute}/edit/${studentId}`;
+export const deleteStudentApi = (studentId) => `${studentRoute}/delete/${studentId}`;
 
 // New version routes
-const checkStudentExistsApi = `${studentRoute}/checkStudentExists`;
-const addStudentToMultipleClassApi = (classId) => `${studentRoute}/addStudentToMultipleClass/${classId}`;
+export const checkStudentExistsApi = `${studentRoute}/checkStudentExists`;
+export const addStudentToMultipleClassApi = (classId) => `${studentRoute}/addStudentToMultipleClass/${classId}`;
 
-//User Api
-const updateUserInfoApi = `${userRoute}/update`;
-const deleteUserApi = (userId) => `${userRoute}/delete/${userId}`;
-const getAllUsersApi = `${userRoute}/all`;
-const getUserByIdApi = (userId) => `${userRoute}/${userId}`;
+// User Api
+export const updateUserInfoApi = `${userRoute}/update`;
+export const deleteUserApi = (userId) => `${userRoute}/delete/${userId}`;
+export const getAllUsersApi = `${userRoute}/all`;
+export const getUserByIdApi = (userId) => `${userRoute}/${userId}`;
+
